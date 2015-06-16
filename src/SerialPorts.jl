a49dfbd7bbeff5c7a3d762071229d925befd623b
Steve Kelly <kd2cca@gmail.com>
2015-06-15 21:16:54 -0500

    first sketch

--- src/SerialPorts.jl
+++ src/SerialPorts.jl
@@ -1,5 +1,38 @@
 module SerialPorts
 
-# package code goes here
+using PyCall
+
+@pyimport serial
+
+type SerialException <: Base.Exception end
+
+immutable SerialPort
+    port
+    baudrate
+    bytesize
+    parity
+    stopbits
+    timeout
+    xonxoff
+    rtscts
+    write_timeout
+    dsrdtr
+    inter_char_timeout
+    python_ptr
+end
+
+function Base.call(::Type{SerialPort}, port, baudrate)
+    py_ptr = serial.Serial(port, baudrate)
+    SerialPort(port, baudrate, 1, 1, 1, 1, 1, 1, 1, 1, 1, py_ptr)
+end
+
+function Base.open(serialport::SerialPort)
+    serialport.python_ptr[:open]()
+    return serialport
+end
+
+function Base.write(serialport::SerialPort, str)
+    serialport.python_ptr[:write](str)
+end
 
 end # module
