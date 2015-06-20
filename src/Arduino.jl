0a07c143017310b58f66b2135db28d49e89be9db
Steve Kelly <kd2cca@gmail.com>
2015-06-20 17:06:19 -0500

    fix type signature

--- src/Arduino.jl
+++ src/Arduino.jl
@@ -5,7 +5,7 @@ using SerialPorts
 """
 Do the magic to reset and Arduino board.
 """
-function reset(s::SerialPorts)
+function reset(s::SerialPort)
     setDTR(s, false)
     sleep(1)
     setDTR(s, true)
