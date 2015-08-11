5c22c7468a0b594e2390bb133c6ed5c6e64297e7
Steve Kelly <kd2cca@gmail.com>
2015-08-11 01:39:08 -0500

    simplify install

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,8 @@
-using BinDeps
 using Compat
 
-@BinDeps.setup
-
-pyser = library_dependency("python-serial")
-
-provides(AptGet, "python-serial", pyser)
-
-@BinDeps.install @compat(Dict(:pyser => :serial))
+@linux_only begin
+    lsb = split(readchomp(pipe(`lsb_release -ds`, stderr=DevNull)))
+    if "Debian" in lsb || "Ubuntu" in lsb
+        println(readall(`sudo apt-get install python-serial`))
+    end
+end
