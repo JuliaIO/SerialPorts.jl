7397f9dcb3e199785065c67ccc769111cceb809e
Steve Kelly <kd2cca@gmail.com>
2015-09-21 20:26:30 -0400

    try to use BinDeps

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,7 @@
+using BinDeps
+@BinDeps.setup
+pyserial = library_dependency("pyserial", aliases = ["serial","pyserial"])
+
 using Conda
 Conda.add("pyserial")
+provides(Conda.Manager, "pyserial", pyserial)
