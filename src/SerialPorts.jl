59a0b593ad8c165d94c5f104ff40f5ce88cb7150
Steve Kelly <kd2cca@gmail.com>
2015-11-14 14:55:42 -0500

    [bugfix] Fix bug introduced when reverting 207f454d9fa7b1c2a299fa33aa11ca3c561e4919.
    closes #9

--- src/SerialPorts.jl
+++ src/SerialPorts.jl
@@ -50,13 +50,20 @@ end
 
 function serialport(port, baudrate)
     py_ptr = PySerial[:Serial](port, baudrate)
-    SerialPort(port, baudrate, 1, 1, 1, 1, 1, 1, 1, 1, 1, py_ptr)
+    SerialPort(port,
+               baudrate,
+               py_ptr[:bytesize],
+               py_ptr[:parity],
+               py_ptr[:stopbits],
+               py_ptr[:timeout],
+               py_ptr[:xonxoff],
+               py_ptr[:rtscts],
+               py_ptr[:dsrdtr], py_ptr)
 end
 
 if VERSION >= v"0.4-"
     function Base.call(::Type{SerialPort}, port, baudrate)
-        py_ptr = PySerial[:Serial](port, baudrate)
-        SerialPort(port, baudrate, 1, 1, 1, 1, 1, 1, 1, 1, 1, py_ptr)
+        serialport(port, baudrate)
     end
 end
 
