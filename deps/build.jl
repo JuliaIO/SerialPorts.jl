32b3f71f84cfaed94a5aee683b96a9a7e42cefec
Steve Kelly <kd2cca@gmail.com>
2019-01-03 23:20:08 -0500

    more testing

--- deps/build.jl
+++ deps/build.jl
@@ -1,38 +1,34 @@
 using Base.Sys
 using PyCall
 
-# Change that to whatever packages you need.
-PACKAGES = ["serial"]
+pip = run(`pip -V`)
+pip3 = run(`pip3 -V`)
 
-# Import pip
-try
-    @pyimport pip
-catch
-    # If it is not found, install it
-    get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
-    download("https://bootstrap.pypa.io/get-pip.py", get_pip)
-    run(`$(PyCall.python) $get_pip --user`)
-end
+pip.exitcode && run(`pip install serial`)
+pip3.exitcode && run(`pip3 install serial`)
 
-@pyimport pip
-args = UTF8String[]
-if haskey(ENV, "http_proxy")
-    push!(args, "--proxy")
-    push!(args, ENV["http_proxy"])
-end
-push!(args, "install")
-push!(args, "--user")
-append!(args, PACKAGES)
+# # Change that to whatever packages you need.
+# PACKAGES = ["serial"]
 
-pip.main(args)
+# # Import pip
+# try
+#     @pyimport pip
+# catch
+#     # If it is not found, install it
+#     get_pip = joinpath(dirname(@__FILE__), "get-pip.py")
+#     download("https://bootstrap.pypa.io/get-pip.py", get_pip)
+#     run(`$(PyCall.python) $get_pip --user`)
+# end
 
-# using PyCall
+# @pyimport pip
+# args = UTF8String[]
+# if haskey(ENV, "http_proxy")
+#     push!(args, "--proxy")
+#     push!(args, ENV["http_proxy"])
+# end
+# push!(args, "install")
+# push!(args, "--user")
+# append!(args, PACKAGES)
 
-# PyCall.conda && exit()
+# pip.main(args)
 
-# pip_exists = run(`pip -V`)
-# pip3_exists = run(`pip3 -V`)
-
-# @debug "hello"
-# @debug pip_exists
-# @debug pip3_exists
